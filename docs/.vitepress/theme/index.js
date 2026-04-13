156acd9319f70415c09eb326ecc2ca8161953c3a
Donny Yang <donny@qmdeve.com>
2026-04-13 19:30:42 +0800

    update

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
 import Projects from './components/Projects.vue'
-import AdUnit from './components/AdUnit.vue'
 
 export default {
   extends: DefaultTheme,
